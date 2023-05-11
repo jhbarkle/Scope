52286ad47ba42cd4e2a435f0ee806b30f946014b
James Barkley Jr <98111214+jhbarkle@users.noreply.github.com>
2023-05-11 14:24:05 -0400

    added stylesheet for homepage

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -1,8 +1,10 @@
+import styles from "./HomePage.module.scss";
+
 const HomePage = () => {
   return (
-    <>
+    <div id={styles.home_wrapper}>
       <h1>Home</h1>
-    </>
+    </div>
   );
 };
 
