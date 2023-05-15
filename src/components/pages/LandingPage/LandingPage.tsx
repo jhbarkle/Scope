ee5d15d1bdeb2da8df564f5f4aabf4cc195478e8
James Barkley Jr <98111214+jhbarkle@users.noreply.github.com>
2023-05-15 14:25:03 -0400

    completed basic responsive structure

--- src/components/pages/LandingPage/LandingPage.tsx
+++ src/components/pages/LandingPage/LandingPage.tsx
@@ -4,7 +4,10 @@ import styles from "./LandingPage.module.scss";
 const LandingPage = () => {
   return (
     <main id={styles.main_wrapper}>
-      <section id={styles.main_content_section}></section>
+      <section id={styles.main_content_section}>
+        <section id={styles.info_content}></section>
+        <section id={styles.image_content}></section>
+      </section>
       <footer></footer>
     </main>
   );
