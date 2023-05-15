7453e55bc1f173ae0c637eed78ec35040e1d5462
James Barkley Jr <98111214+jhbarkle@users.noreply.github.com>
2023-05-15 15:02:57 -0400

    added info elements

--- src/components/pages/LandingPage/LandingPage.tsx
+++ src/components/pages/LandingPage/LandingPage.tsx
@@ -5,12 +5,24 @@ const LandingPage = () => {
   return (
     <main id={styles.main_wrapper}>
       <section id={styles.main_content_section}>
-        <section id={styles.info_content}></section>
+        <section id={styles.info_content}>
+          <section id={styles.info_text_container}>
+            <h1>Learn more about your favorite artists.</h1>
+            <p>
+              Learn about your favorite music artist’s discography,
+              automatically create playlists around these artists, and even view
+              your Spotify stats!
+            </p>
+            <Link to="/home">
+              <button>Login to Spotify</button>
+            </Link>
+          </section>
+        </section>
         <section id={styles.image_content}>
           <img src="/icons/landing-page-image.jpg" alt="" />
         </section>
       </section>
-      <footer></footer>
+      <footer>Powered by Spotify & ChatGPT</footer>
     </main>
   );
 };
