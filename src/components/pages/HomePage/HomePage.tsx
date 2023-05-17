e0aeb8ba1c99091af9b4079ff5846c9ae712d41c
James Barkley Jr <98111214+jhbarkle@users.noreply.github.com>
2023-05-17 12:06:35 -0400

    added new logic isLoading logic

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -3,33 +3,19 @@ import styles from "./HomePage.module.scss";
 import { authorizeAndGatherUserData } from "../../../services/spotify";
 
 const HomePage = () => {
-  const [displayName, setDisplayName] = useState(
-    localStorage.getItem("display_name") ?? "User"
-  );
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchProfileData = async () => {
-      // if (
-      //   localStorage.getItem("display_name") === undefined ||
-      //   localStorage.getItem("display_name") === null
-      // ) {
-      //   console.log("Fetching User Data");
-      //   await authorizeAndGatherUserData();
-      // } else {
-      //   console.log("Already Logged In");
-      //   return;
-      // }
       await authorizeAndGatherUserData();
-
-      // const displayName = localStorage.getItem("display_name");
-      // setDisplayName(displayName ?? "Anon");
+      setIsLoading(false);
     };
 
     fetchProfileData();
   }, []);
 
-  return displayName === "User" ? (
-    <div>
+  return isLoading ? (
+    <div id={styles.loading}>
       <img src="/loading.gif" alt="Loading" />
     </div>
   ) : (
